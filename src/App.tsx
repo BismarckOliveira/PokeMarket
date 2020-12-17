2f47ed2429606b75f3a3dad0d3cc5d84096605be
BismarckOliveira <bismarckoliver@hotmail.com>
2020-12-16 23:46:45 -0300

    Html body

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import SignIn from './pages/signIn';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>Hello word</h1>;
-
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
 export default App;
